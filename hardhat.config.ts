c32adbdd8c64d2769f817af97a0ca325438ae98e
Arham Irfan <arham.irfaann@gmail.com>
2024-01-23 12:56:46 +0500

    comment with test coverage job updated

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -111,7 +111,7 @@ const config: ExtendedHardhatUserConfig = {
     enabled: true,
     currency: "USD",
     gasPrice: 21,
-    outputFile: 'gas-report.txt',
+    outputFile: 'report.json',
     showMethodSig: true,
     noColors: true,
   },
